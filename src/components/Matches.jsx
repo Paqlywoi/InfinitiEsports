71bfb2fe4b2a7164c2832bca0e15a154b3e5b805
Haziq Fakhri <haziqfakhriadnan@gmail.com>
2026-05-07 09:26:22 +0800

    7 mei 2026

--- src/components/Matches.jsx
+++ src/components/Matches.jsx
@@ -5,6 +5,7 @@ import { LayoutGrid, History, BarChart3, Activity, Target, ShieldAlert } from 'l
 const matchData = {
   upcoming: [],
   results: [
+    { id: 11, vs: "Jebat Boti", score: "2 - 1", status: "WIN", event: "Friendly" },
     { id: 10, vs: "Kroni K902", score: "2 - 0", status: "WIN", event: "Friendly" },
     { id: 9, vs: "Mega Autotech", score: "1 - 2", status: "LOSS", event: "Friendly" },
     { id: 8, vs: "Madfox Chaos", score: "0 - 1", status: "LOSS", event: "MAL Qualifier S5" },
